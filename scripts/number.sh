5daae38f3473826140aa4a49bf23ac3e39da2f0b
Woody Huang <woody1573@163.com>
2016-12-17 15:35:39 +0800

    helper script to query the number of patents

---
+++ scripts/number.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd "$( dirname "${BASE_SOURCE[0]}")"
+cd ..
+
+if [ $# -lt 1 ]; then
+    exit 1
+fi
+
+YEAR=$1
+
+source ../env/bin/activate
+python -c "from uspto_fast.models import *; print new_session().query(Patent).filter_by(apply_year=${YEAR}).count()"
